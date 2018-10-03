a8268497b758752decd137d09bf17960e85c1bdd
Alex Zdatny <alexzdatny@gmail.com>
2018-10-03 19:03:32 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,9 +1,6 @@
 class SecretsController < ApplicationController
   before_action :require_login
 
-  def create
-  end
-
   def show
     redirect_to controller: 'sessions', action: 'new' unless current_user
   end
